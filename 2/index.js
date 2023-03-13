dce7c085f60c32a8c0c7b97296f7d051e5fe413f
Gromovoi Stanislav <stasgromovoi@gmail.com>
2023-03-13 23:13:06 +0000

    merge, concat

--- 2/index.js
+++ 2/index.js
@@ -367,32 +367,48 @@ evens$.subscribe(x => console.log('evens', x));
 	
 	
 	
-///////////////////
+//////////////////////////////////////////////////////
 // const s1 = rxjs.of('Hello')
 // const s2 = rxjs.of('world')
+//rxjs.merge(s1, s2).subscribe(x => console.log(x));
+// Logs:
+// Hello
+// world
+
 
 // const s1 = rxjs.interval(1000).pipe(rxjs.map(x => 's1: ' + x))
 // const s2 = rxjs.interval(500).pipe(rxjs.map(x => 's2: ' + x))
 // const s3 = rxjs.interval(100).pipe(rxjs.map(x => 's2: ' + x))
 // const concurrent = 2;
-// from.merge(s1, s2, s3, concurrent).pipe(rxjs.take(12)).subscribe(x => console.log(x));
+// rxjs.merge(s1, s2, s3, concurrent).pipe(rxjs.take(12)).subscribe(x => console.log(x));
 
 
 
 //// const higherOrder = rxjs.range(1, 3).pipe(rxjs.map(x => rxjs.range(x, 3)))
 // const higherOrder = rxjs.range(1, 3).pipe(rxjs.map(x => rxjs.interval(1000).pipe(rxjs.take(4))))
 // higherOrder.pipe(rxjs.mergeAll()).subscribe(x => console.log(x));
+//// Logs: 1, 2, 3, 2, 3, 4, 3, 4, 5
+// Logs: 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3
 
 
-///////////////////
+//////////////////////////////////////////////
+// const s1 = rxjs.of('Hello')
+// const s2 = rxjs.of('world')
+//rxjs.concat(s1, s2).subscribe(x => console.log(x));
+// Logs:
+// Hello
+// world
+
 // const s1 = rxjs.from([1,2,3])
 // const s2 = rxjs.from([4,5,6])
 // rxjs.concat(s1, s2).subscribe(x => console.log(x));
+// Logs: 1, 2, 3, 4, 5, 6
 
 //// const higherOrder = rxjs.range(1, 3).pipe(rxjs.map(x => rxjs.range(x, 3)))
 // const higherOrder = rxjs.range(1, 3).pipe(rxjs.map(x => rxjs.interval(1000).pipe(rxjs.take(4))))
 // higherOrder.pipe(rxjs.concatAll()).subscribe(x => console.log(x));
-
+//// Logs: 1, 2, 3, 2, 3, 4, 3, 4, 5
+// Logs: 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3
 
 
 //////////////////////////////////////
